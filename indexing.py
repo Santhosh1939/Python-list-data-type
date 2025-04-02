06038b6be0d13d1de9919fc19b38e5e8f0e2a4f7
SANTHOSH KUMAR GOWDA <gowda.santhu1939@gmail.com>
2025-04-01 20:12:59 -0400

    Add files via upload

---
+++ indexing.py
@@ -0,0 +1,92 @@
+
+# 1)Write Python code to reverse the order of elements in the given list my_list .
+# Print the reversed list.
+
+# my_list = [10, 20, 30, 40, 50, 11]
+# print(my_list[-1::-1])
+
+# 2)Given two lists list1 and list2 , find and print the common elements between
+# them.
+# common_elements=[]
+# list1 = [1, 2, 3, 4, 5]
+# list2 = [4, 5, 6, 7, 8]
+# for i in list1:
+#     for j in list2:
+#         if i == j :
+#             common_elements.append(i)
+# print(common_elements)
+
+# 3)Create a new list unique_list containing only the unique elements from the
+# given list original_list . Print the unique list.
+
+# unique_list=[]
+# original_list = [1, 2, 2, 3, 4, 4, 5]
+# for i in original_list:
+#     if i not in unique_list:
+#         unique_list.append(i)
+# print(unique_list)
+
+#4)Remove duplicate elements from the given list duplicated_list and print the list
+# without duplicates while preserving the order.
+# without_dup=[]
+# duplicated_list = [1, 2, 2, 3, 4, 4, 5]
+# for i in duplicated_list:
+#     if i not in without_dup:
+#         duplicated_list.append(i)
+# print(without_dup)
+
+
+#################################################################################################
+
+
+# Write a Python script that concatenates two lists and prints the result.
+
+# list_1=[1,2,3,4,5]
+# list_2=[4,5,6,7,8]
+# result=list_1+list_2
+# print(result)
+
+# Write a Python script that repeats a list three times and prints the result.
+# list_1=[1,2,3,4,5]
+# result= (list_1*3)
+# print(result)
+
+# Exercise 3: List Removal
+# Write a Python script that removes the elements at even indices from a list.
+
+# list_1=[1,2,3,4,5,6]
+# for i in list_1:
+#     if i%2 != 0:
+#         print(i)
+
+
+# Exercise 4: List Insertion
+# Write a Python script that inserts the numbers 10, 11, and 12 at the beginning of a list
+
+# list_1=[1,2,3,4,5,6]
+# list_1.insert(0,10)
+# list_1.insert(1,11)
+# list_1.insert(2,12)
+# print(list_1)
+
+#############################################################################
+
+# 1. Square Numbers: Create a list of squares of numbers from 1 to 10.        
+# list_1=[1,2,3,4,5,6,7,8,9,10]
+# for i in list_1:
+#     print(i*i)
+# generate a list.
+# 2. Even Numbers: Generate a list of even numbers from 1 to 20.
+# for i in range(1,21):
+#     if i%2 == 0:
+#         print(i)
+
+# 3. Words Lengths: Given a list of words, create a list containing the lengths of
+# each word.
+# words = ["apple", "banana", "cherry", "date"]
+# my_list =[]
+# for i in words:
+#     my_list.append(len(i))
+#     print(my_list) #remove indentation
+
+
